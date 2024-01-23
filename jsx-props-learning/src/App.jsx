662339285dc7687b4cec218dff6330d52aba1d03
Shreyansh <katiyarshreyansh0@gmail.com>
2024-01-23 22:39:34 +0530

    second commit

---
+++ jsx-props-learning/src/App.jsx
@@ -0,0 +1,11 @@
+import Header from "./Header"
+import React from "react"
+function App(){
+  return(
+  <React.Fragment>
+    <h1>Hello from App</h1>
+    <Header/>
+  </React.Fragment>
+  )
+}
+export default App
