66dac7c173f29d6e75f6a4ec005544c08bd0c106
Shreyansh <katiyarshreyansh0@gmail.com>
2024-01-24 09:03:17 +0530

    thrid commit

--- jsx-props-learning/src/App.jsx
+++ jsx-props-learning/src/App.jsx
@@ -1,12 +1,19 @@
 import Header from "./Header"
 import React from "react"
+const styleObject={
+  border:"2px solid green",
+  backgroundColor:"black",
+  color:"white",
+  padding:"1rem",
+}
+
 function App(){
   return(
-  <React.Fragment>
+  <div style={styleObject}>
     <h1>Hello from App</h1>
     {/* <h2>maths : {4 + +'5'}</h2> */}
     <Header/>
-  </React.Fragment>
+  </div>
   )
 }
 export default App
